699525f34616d03eadf8f6b15de648593ff4b0f3
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-06-21 18:39:31 +0400

    Binary Search

---
+++ app/src/main/java/com/sargis/khlopuzyan/algorithmsgfg/searching/BinarySearch.kt
@@ -0,0 +1,30 @@
+package com.sargis.khlopuzyan.algorithmsgfg.searching
+
+/**
+ * Created by Sargis Khlopuzyan, on 6/21/2021.
+ *
+ * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
+ */
+fun main() {
+    val array = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result = binarySearch(array, x = 9)
+    if (result == -1)
+        println("Element is not present in array")
+    else
+        println("Element is present at index $result")
+}
+
+fun binarySearch(array: Array<Int>, left: Int = 0, right: Int = array.size - 1, x: Int): Int {
+
+    if (left > right) return -1
+
+    val mid = left + (right - left) / 2
+    println("Inside binarySearch => mid : $mid")
+
+    return when {
+        x == array[mid] -> mid
+        x < array[mid] -> binarySearch(array, left, mid - 1, x)
+        x > array[mid] -> binarySearch(array, mid + 1, right, x)
+        else -> -1
+    }
+}
--- app/src/main/java/com/sargis/khlopuzyan/algorithmsgfg/searching/LinearSearch.kt
+++ app/src/main/java/com/sargis/khlopuzyan/algorithmsgfg/searching/LinearSearch.kt
@@ -7,7 +7,7 @@ package com.sargis.khlopuzyan.algorithmsgfg.searching
  */
 fun main() {
     val array = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val result = linearSearch2(array, 9)
+    val result = linearSearch2(array, 5)
 //    val result = linearSearch1(array, 7)
     if (result == -1)
         println("Element is not present in array")
@@ -17,12 +17,15 @@ fun main() {
 
 /**
  * Improve Linear Search Worst-Case Complexity
+ * 1) if element Found at last  O(n) to O(1)
+ * 2) if element Not found O(n) to O(n/2)
  * */
 fun linearSearch2(array: Array<Int>, x: Int): Int {
     var left = 0
     var right = array.size - 1
 
     while (left <= right) {
+        println("left: $left")
         when (x) {
             array[left] -> return left
             array[right] -> return right
